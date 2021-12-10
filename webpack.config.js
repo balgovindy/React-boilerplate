454d7fdb023f468dedd95bfa123b34bb6e5a9151
balgovindy <you@example.com>
2021-12-10 17:14:54 +0530

    React-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+    entry: SRC_DIR + '/index.js',
+    output: {
+        path: DIST_DIR,
+        filename: 'main.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: SRC_DIR + '/index.html'
+        })
+    ]
+}
